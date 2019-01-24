f380ac834efadb634f755eacce1b8c93a492caee
Solomon <ksolomon@andrews.edu>
2019-01-24 15:55:20 -0500

    initial commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    MeshRenderer cubeRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        cubeRenderer.enabled = false;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        cubeRenderer.enabled = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
