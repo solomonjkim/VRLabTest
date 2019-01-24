a4e3d6e1d4e6e9a626e4d70887c9b2b4e4c6499c
Solomon Kim <ksolomon@inside.andrews.edu>
2019-01-24 16:38:13 -0500

    1st Lab

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -14,6 +14,7 @@ public class CubeController : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         cubeRenderer.enabled = false;
+	//I did it
     }
 
     void OnTriggerExit(Collider other)
